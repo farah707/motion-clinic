e1b6b563bbf8204fe4daa393d0f869ed5ccfc710
Farah-Tarek101 <farahtarek707@gmail.com>
2025-06-30 18:40:57 +0300

    Fix case sensitivity in AboutUs.css import for Vercel deployment

--- frontend/src/Pages/AboutUs/AboutUs.jsx
+++ frontend/src/Pages/AboutUs/AboutUs.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import "./AboutUs.css";
+import "./Aboutus.css";
 
 const AboutUs = () => {
   const { t } = useTranslation(); // Hook for translations
